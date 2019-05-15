c0ad1441a0884d45d266c566bad2f8bfe24add07
elmurotyangiboev <elmurotyangiboev@elmurots-imac.fios-router.home>
2019-05-15 17:18:50 -0400

    added new MavenJava project for practice purpose

---
+++ MavenJava/src/main/java/qaClickAcademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaClickAcademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void nativeAndroidApp (){
+
+        System.out.println("nativeAndroidApp");
+    }
+
+    @Test
+    public void nativeIOSApp(){
+
+        System.out.println("nativeIOSApp");
+
+    }
+}
